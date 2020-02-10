ee4d58dcd75bad8cb752478bfb51a697e6e8d99f
maks <maxdabrowski93@o2.pl>
2020-02-10 23:04:25 +0100

    Updates

---
+++ precache-manifest.3b29f8f3b899bd02ca46f8955dc4d272.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40a5c5f599ad2d03c47959b01352d8c",
+    "url": "/tabulatory_gitarowe/index.html"
+  },
+  {
+    "revision": "caca8134c647e2fb56fc",
+    "url": "/tabulatory_gitarowe/static/css/main.dc6947d0.chunk.css"
+  },
+  {
+    "revision": "6f3303750d389fecd0a6",
+    "url": "/tabulatory_gitarowe/static/js/2.7e681a74.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tabulatory_gitarowe/static/js/2.7e681a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caca8134c647e2fb56fc",
+    "url": "/tabulatory_gitarowe/static/js/main.6f51881a.chunk.js"
+  },
+  {
+    "revision": "7bc5c87fab5af60e632f",
+    "url": "/tabulatory_gitarowe/static/js/runtime-main.84602ef4.js"
+  },
+  {
+    "revision": "467834220290462aa179265f14ef56a7",
+    "url": "/tabulatory_gitarowe/static/media/guitar.46783422.jpg"
+  }
+]);
